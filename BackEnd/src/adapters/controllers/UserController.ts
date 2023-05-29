b3325859902c2775f627c3200addcb7689fb027d
joaovalgas <55608513+joaovalgas@users.noreply.github.com>
2023-05-29 10:58:12 -0300

    Add validação de dados do usuario

--- BackEnd/src/adapters/controllers/UserController.ts
+++ BackEnd/src/adapters/controllers/UserController.ts
@@ -3,8 +3,25 @@ import { UserRepository } from '../../ports/UserRepository';
 
 export class UserController {
   constructor(private userRepository: UserRepository) {}
+  
 
   async register(id: string, name: string, email: string, password: string): Promise<void> {
+   
+    // Validar os dados inseridos
+    if (!id || !name || !email || !password) {
+      throw new Error('Todos os campos devem ser preenchidos.');
+    }
+    // Verificar se o email é válido
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      throw new Error('O email fornecido não é válido.');
+    }
+
+    const existingUser = await this.userRepository.findByEmail(email);
+    if (existingUser) {
+      throw new Error('Já existe um usuário com esse email.');
+    }
+
     const user = new User(id,name, email, password);
     await this.userRepository.save(user);
   }
@@ -17,4 +34,9 @@ export class UserController {
       return null;
     }
   }
+
+  async getUserById(id: string): Promise<User | null> {
+    const user = await this.userRepository.findById(id);
+    return user;
+  }  
 }
--- BackEnd/src/ports/UserRepository.ts
+++ BackEnd/src/ports/UserRepository.ts
@@ -3,4 +3,5 @@ import { User } from '../domain/entities/User';
 export interface UserRepository {
     save(user: User): Promise<void>;
     findByEmail(email: string): Promise<User | null>;
+    findById(id: string): Promise<User | null>;
 }
