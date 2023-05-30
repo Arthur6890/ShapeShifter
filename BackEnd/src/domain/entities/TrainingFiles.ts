ddbab3f40c325f253444e1db6f66c856accb318e
joaovalgas <55608513+joaovalgas@users.noreply.github.com>
2023-05-29 23:19:54 -0300

    Add: Criação do arquivo de classe TrainingFiles

---
+++ BackEnd/src/domain/entities/TrainingFiles.ts
@@ -0,0 +1,5 @@
+export class trainingFile{ 
+    constructor(public Type: string, public exercise: string, public sets: number,
+                public repetitions: number, public exerciseImage: string){}
+
+}
