db2648bf69fce52e0a8f11b951873f88ca35d828
jackjacone <jaconeufmg@gmail.com>
2023-05-29 22:27:24 -0300

    Update MongoDBUserRepository.ts
    Criação do Repositório de Usuário do Banco de Dados

--- BackEnd/src/adapters/repositories/MongoDBUserRepository.ts
+++ BackEnd/src/adapters/repositories/MongoDBUserRepository.ts
@@ -1,16 +1,45 @@
-import { User } from '../../domain/entities/User';
-import { UserRepository } from '../../ports/UserRepository';
-/*
-//import { MongoDB } from '../../infrastructure/db/MongoDB';
+const { MongoClient } = require('mongodb');
 
-export class MongoDBUserRepository implements UserRepository {
-  constructor(private db: MongoDB) {}
+const url = 'mongodb://localhost:27017';
+const dbName = 'nome_do_banco_de_dados';
 
-  async save(user: User): Promise<void> {
-    // Implementação para salvar o usuário no MongoDB
+async function createUserCollection() {
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+
+    // Criação da coleção "users"
+    const usersCollection = db.collection('users');
+
+    // Definição do índice único para o campo "userName"
+    await usersCollection.createIndex({ userName: 1 }, { unique: true });
+
+    console.log('Coleção "users" criada com sucesso!');
+
+    // Exemplo de documento de usuário
+    const user = {
+      userName: 'john123',
+      password: 'senha123',
+      email: 'john@example.com',
+      passwordTip: 'Dica de senha',
+      name: 'John Doe',
+      goal: 'Perder peso',
+      daysInaRowTraining: 7,
+      daysInaMonthTraining: 20,
+      trainingFiles: []
+    };
+
+    // Inserir o documento do usuário na coleção
+    await usersCollection.insertOne(user);
+
+    console.log('Documento de usuário inserido com sucesso!');
+  } catch (err) {
+    console.error('Erro ao criar coleção ou inserir documento:', err);
+  } finally {
+    client.close();
   }
+}
 
-  async findByEmail(email: string): Promise<User | null> {
-    // Implementação para encontrar um usuário no MongoDB pelo email
-  }
-}*/
+createUserCollection();
