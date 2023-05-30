af64daeee8de3ec2f298ed8a18df1e9f5cf37e78
joaovalgas <55608513+joaovalgas@users.noreply.github.com>
2023-05-30 00:18:28 -0300

    Update TrainingFiles.ts

--- BackEnd/src/domain/entities/TrainingFiles.ts
+++ BackEnd/src/domain/entities/TrainingFiles.ts
@@ -1,4 +1,4 @@
-export class trainingFile{ 
+export class TrainingFiles{ 
     constructor(public Type: string, public exercise: string, public sets: number,
                 public repetitions: number, public exerciseImage: string){}
 
