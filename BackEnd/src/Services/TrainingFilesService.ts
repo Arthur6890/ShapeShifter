50c23832200ff5e309c7de663be6e40c5bd1d0f9
joaovalgas <55608513+joaovalgas@users.noreply.github.com>
2023-05-30 23:46:23 -0300

    Update TrainingFilesService.ts

--- BackEnd/src/Services/TrainingFilesService.ts
+++ BackEnd/src/Services/TrainingFilesService.ts
@@ -3,13 +3,9 @@ import { ITrainingFilesService } from "./ITrainingFilesService";
 import { createTrainingFilesCollection } from "../adapters/repositories/MongoDBTrainingFilesRepository";
 
 export default class TrainingFileService implements ITrainingFilesService{
-    findById(userId: string): Promise<ITrainingFiles | null> {
-        throw new Error("Method not implemented.");
-    }
     async createTrainingFile(trainingFile: ITrainingFiles): Promise<ITrainingFiles> {
         await createTrainingFilesCollection(trainingFile);
         return trainingFile;
-        throw new Error("Method not implemented.");
     }
 
 }
