8e7b47edfe2685a30c7ddce0a7415b58e9fa24d2
Alexis Mariz <adgmariz@gmail.com>
2023-06-26 14:21:27 -0300

    update babel

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
-    "@babel/preset-env",
-    "@babel/preset-react",
-    '@babel/preset-typescript',
-  ],
-};
